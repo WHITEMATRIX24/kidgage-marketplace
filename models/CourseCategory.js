edd2564581b0fdacd13559f1aac6c5550d69bf62
sweetysara <sweetysarasunny@gmail.com>
2024-10-16 11:29:10 +0530

    first commit

--- models/CourseCategory.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const courseCategorySchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  image: { type: String, required: true }, // Base64 encoded image
-});
-
-const CourseCategory = mongoose.model('CourseCategory', courseCategorySchema);
-module.exports = CourseCategory;
